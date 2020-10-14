c5376e29d20328a885eeeda16f618e12db3a0fd2
Alex Maclean <alex@coreblue.co.uk>
2020-10-14 16:15:12 +0100

    Add request validation for the creation of Threads and Replies and add the appropriate tests for this

--- tests/Feature/Forum/ParticipateTest.php
+++ tests/Feature/Forum/ParticipateTest.php
@@ -33,4 +33,16 @@ class ParticipateTest extends TestCase
         $this->post('/threads/some-channel/1/replies', [])
             ->assertRedirect('/login');
     }
+
+    /** @test */
+    public function a_reply_requires_a_body()
+    {
+        $this->signIn();
+
+        $thread = create(Thread::class);
+        $reply = make(Reply::class, ['body' => null]);
+
+        $this->post($thread->path() . '/replies', $reply->toArray())
+            ->assertSessionHasErrors('body');
+    }
 }
--- tests/Feature/Threads/CreateTest.php
+++ tests/Feature/Threads/CreateTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Threads;
 
+use App\Models\Channel;
 use App\Models\Reply;
 use App\Models\Thread;
 use App\Models\User;
@@ -12,17 +13,33 @@ use Tests\TestCase;
 class CreateTest extends TestCase
 {
     /** @test */
+    public function authenticated_user_can_view_create_new_forum_threads_page()
+    {
+        $this->signIn();
+
+        $this->get('threads/create')
+            ->assertViewIs('threads.create');
+    }
+
+    /** @test */
     public function an_authenticated_user_can_create_new_forum_threads()
     {
         $this->signIn();
 
-        $thread = create(Thread::class);
+        $thread = make(Thread::class);
 
-        $this->post('/threads', $thread->toArray());
+        $response = $this->post('/threads', $thread->toArray());
 
-        $this->get($thread->path())
+        $this->get($response->headers->get('Location'))
             ->assertSee($thread->title)
             ->assertSee($thread->body);
+    }
+
+    /** @test */
+    public function an_unauthenticated_cannot_view_create_new_forum_threads_page()
+    {
+        $this->get('threads/create')
+            ->assertRedirect('/login');
     }
 
     /** @test */
@@ -41,18 +58,37 @@ class CreateTest extends TestCase
     }
 
     /** @test */
-    public function authenticated_user_can_view_create_new_forum_threads_page()
+    public function a_thread_requires_a_title()
     {
-        $this->signIn();
-
-        $this->get('threads/create')
-            ->assertViewIs('threads.create');
+        $this->publishThread(['title' => null])
+            ->assertSessionHasErrors('title');
     }
 
     /** @test */
-    public function an_unauthenticated_cannot_view_create_new_forum_threads_page()
+    public function a_thread_requires_a_body()
     {
-        $this->get('threads/create')
-            ->assertRedirect('/login');
+        $this->publishThread(['body' => null])
+            ->assertSessionHasErrors('body');
+    }
+
+    /** @test */
+    public function a_thread_requires_a_valid_channel()
+    {
+        create(Channel::class)->count(2);
+
+        $this->publishThread(['channel_id' => null])
+            ->assertSessionHasErrors('channel_id');
+
+        $this->publishThread(['channel_id' => 999])
+            ->assertSessionHasErrors('channel_id');
+    }
+
+    private function publishThread($overrides = [])
+    {
+        $this->signIn();
+
+        $thread = make(Thread::class, $overrides);
+
+        return $this->post('/threads', $thread->toArray());
     }
 }
