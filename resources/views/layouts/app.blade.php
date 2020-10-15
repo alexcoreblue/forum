4abd443c8c3c68441545232a93d6d0a97eb11ce2
Alex Maclean <alex@coreblue.co.uk>
2020-10-15 10:58:04 +0100

    Add the Choose Channel option to the publish Thread form and add client and server side validation for the fields

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,8 +35,8 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-                        <li><a href="/threads">All Threads</a></li>
-
+                        <li class="nav-item"><a class="nav-link" href="/threads">All Threads</a> </li>
+                        <li class="nav-item"><a class="nav-link" href="/threads/create">New Thread</a> </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 Channels
@@ -47,7 +47,6 @@
                                 @endforeach
                             </div>
                         </li>
-
                     </ul>
 
                     <!-- Right Side Of Navbar -->
