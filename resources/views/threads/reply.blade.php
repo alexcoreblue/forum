49801fae3a61eeb14db29597121f4518e025956d
Alex Maclean <alex@coreblue.co.uk>
2020-10-16 09:38:10 +0100

    Add the favourite button allowing a User to Favourite a Reply

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,6 +1,21 @@
 <div class="card">
     <div class="card-header">
-        <a href="#"> {{ $reply->owner->name }} </a> said {{ $reply->created_at->diffForHumans() }}... </div>
+
+        <div class="level">
+            <h5 class="flex">
+                <a href="#"> {{ $reply->owner->name }} </a> said {{ $reply->created_at->diffForHumans() }}...
+            </h5>
+
+            <div>
+                <form method="POST" action="/replies/{{ $reply->id }}/favourites">
+                    @csrf
+                    <button type="submit" class="btn btn-dark" {{ $reply->isFavourited() ? 'disabled' : '' }}>{{ $reply->favourites()->count() }} {{ Str::plural('Favourite', $reply->favourites()->count())}}</button>
+                </form>
+
+            </div>
+        </div>
+
+    </div>
     <div class="card-body">
         {{ $reply->body }}
     </div>
